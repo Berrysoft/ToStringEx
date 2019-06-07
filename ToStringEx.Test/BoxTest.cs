b56ffccc9bbdd157efa532a5e84444a62c07ed83
王宇逸 <Strawberry_Str@hotmail.com>
2019-06-07 16:03:23 +0800

    Add ToStringExBox.

---
+++ ToStringEx.Test/BoxTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToStringEx.Test
+{
+    [TestClass]
+    public class BoxTest
+    {
+        [TestMethod]
+        public void StringFormatTest()
+        {
+            (int, int[], string) t2 = (1, new int[] { 1, 2, 3 }, "abc");
+            Assert.AreEqual("(1, {1, 2, 3}, abc)", $"{ToStringExBox.Create(t2)}");
+        }
+    }
+}
---
+++ ToStringEx/ToStringExBox.cs
@@ -0,0 +1,14 @@
+﻿namespace ToStringEx
+{
+    public struct ToStringExBox
+    {
+        public static ToStringExBox<T> Create<T>(T data) => new ToStringExBox<T> { Data = data };
+    }
+
+    public struct ToStringExBox<T>
+    {
+        public T Data;
+
+        public override string ToString() => Data.ToStringEx();
+    }
+}
