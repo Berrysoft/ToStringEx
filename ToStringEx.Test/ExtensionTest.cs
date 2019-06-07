c006651b59e647918e8729926c0c2d2223f29d24
王宇逸 <Strawberry_Str@hotmail.com>
2019-06-07 15:48:29 +0800

    Add tests.

---
+++ ToStringEx.Test/ExtensionTest.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToStringEx.Test
+{
+    [TestClass]
+    public class ExtensionTest
+    {
+        [TestMethod]
+        public void StringToString()
+        {
+            string a = "Hello world!";
+            Assert.AreEqual(a, a.ToStringEx());
+        }
+
+        [TestMethod]
+        public void TupleToString()
+        {
+            (int, int, string) t = (1, 2, "abc");
+            Assert.AreEqual("(1, 2, abc)", t.ToStringEx());
+            (int, int[], string) t2 = (1, new int[] { 1, 2, 3 }, "abc");
+            Assert.AreEqual("(1, {1, 2, 3}, abc)", t2.ToStringEx());
+            var t3 = Tuple.Create(1, 2, "abc");
+            Assert.AreEqual(t.ToStringEx(), t3.ToStringEx());
+        }
+
+        [TestMethod]
+        public void IEnumerableToString()
+        {
+            int[] a = new int[] { 1, 2, 3, 4, 5 };
+            Assert.AreEqual("{1, 2, 3, 4, 5}", a.ToStringEx());
+            var q = a.Where(i => i % 2 == 0);
+            Assert.AreEqual("{2, 4}", q.ToStringEx());
+        }
+
+        class UserDefinedType
+        {
+            public int Key1 { get; set; }
+            public string Key2 { get; set; }
+            public int[] Key3 { get; set; }
+        }
+        class UserDefinedTypeWithToString
+        {
+            public int Key { get; set; }
+            public override string ToString() => "Hello world!";
+        }
+
+        [TestMethod]
+        public void UserTypeToString()
+        {
+            var c = new UserDefinedType { Key1 = 123, Key2 = "Hello", Key3 = new int[] { 1, 2, 3 } };
+            Assert.AreEqual("{ Key1: 123, Key2: Hello, Key3: {1, 2, 3} }", c.ToStringEx());
+            var c2 = new UserDefinedTypeWithToString { Key = 123 };
+            Assert.AreEqual("Hello world!", c2.ToStringEx());
+        }
+    }
+}
--- ToStringEx/ToStringExtensions.cs
+++ ToStringEx/ToStringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace ToStringEx
 {
@@ -10,22 +11,47 @@ namespace ToStringEx
         public static string ToStringEx(this object obj)
         {
             Type t = obj.GetType();
-            if (obj is IEnumerable source)
-                return IEnumerableToStringEx(source);
-            else if (t.GetMethods().Any(m => m.Name == "ToString" && m.GetParameters().Length == 0))
+            if (t.FullName.StartsWith("System.ValueTuple`"))
+            {
+                return string.Format("({0})", string.Join(", ", EnumerateFields(obj, t).EnumerateTupleLike()));
+            }
+            else if (t.FullName.StartsWith("System.Tuple`"))
+            {
+                return string.Format("({0})", string.Join(", ", EnumerateProperties(obj, t).EnumerateTupleLike()));
+            }
+            else if (t.GetMethod("ToString", Array.Empty<Type>()).DeclaringType == t)
+            {
                 return obj.ToString();
+            }
+            else if (obj is IEnumerable source)
+            {
+                return IEnumerableToStringEx(source);
+            }
             else
             {
-                var props = from p in t.GetProperties()
-                            where p.CanRead && p.GetIndexParameters().Length == 0
-                            select $"{p.Name}: {p.GetValue(obj)}";
-                return string.Format("{{{0}}}", string.Join(", ", props));
+                return string.Format(
+                    "{{ {0} }}",
+                    string.Join(", ", EnumerateFields(obj, t).EnumeratePropLike().Concat(EnumerateProperties(obj, t).EnumeratePropLike())));
             }
         }
 
+        private static IEnumerable<(string Name, string Value)> EnumerateFields(object obj, Type t)
+            => from f in t.GetFields()
+               where f.Attributes.HasFlag(FieldAttributes.Public)
+               select (f.Name, f.GetValue(obj).ToStringEx());
+
+        private static IEnumerable<(string Name, string Value)> EnumerateProperties(object obj, Type t)
+            => from p in t.GetProperties()
+               where p.CanRead && p.GetGetMethod().IsPublic && p.GetIndexParameters().Length == 0
+               select (p.Name, p.GetValue(obj).ToStringEx());
+
+        private static IEnumerable<string> EnumeratePropLike(this IEnumerable<(string Name, string Value)> source)
+            => source.Select(t => $"{t.Name}: {t.Value}");
+
+        private static IEnumerable<string> EnumerateTupleLike(this IEnumerable<(string Name, string Value)> source)
+            => source.Select(t => t.Value);
+
         private static string IEnumerableToStringEx(IEnumerable source)
-        {
-            return string.Format("{{{0}}}", string.Join(", ", source.Cast<object>().Select(obj => obj.ToStringEx())));
-        }
+            => string.Format("{{{0}}}", string.Join(", ", source.Cast<object>().Select(obj => obj.ToStringEx())));
     }
 }
