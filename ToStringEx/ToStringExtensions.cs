6957705db0a0b565e9c9c74b240921fb373eec61
王宇逸 <Strawberry_Str@hotmail.com>
2019-06-07 11:32:17 +0800

    Add simple ToStringEx.

---
+++ ToStringEx/ToStringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToStringEx
+{
+    public static class ToStringExtensions
+    {
+        public static string ToStringEx(this object obj)
+        {
+            Type t = obj.GetType();
+            if (obj is IEnumerable source)
+                return IEnumerableToStringEx(source);
+            else if (t.GetMethods().Any(m => m.Name == "ToString" && m.GetParameters().Length == 0))
+                return obj.ToString();
+            else
+            {
+                var props = from p in t.GetProperties()
+                            where p.CanRead && p.GetIndexParameters().Length == 0
+                            select $"{p.Name}: {p.GetValue(obj)}";
+                return string.Format("{{{0}}}", string.Join(", ", props));
+            }
+        }
+
+        private static string IEnumerableToStringEx(IEnumerable source)
+        {
+            return string.Format("{{{0}}}", string.Join(", ", source.Cast<object>().Select(obj => obj.ToStringEx())));
+        }
+    }
+}
